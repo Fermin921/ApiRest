5fbc215ff0dc3ae2ccbbcee44e9e83a80405fde0
Fermin921 <ferminmartinez921@gmail.com>
2023-09-19 08:08:59 -0500

    Grid

--- Parcial1/Grid/Grid.js
+++ Parcial1/Grid/Grid.js
@@ -3,10 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('http://localhost:8080/usuarios')
     .then(response => response.json())
     .then(data => {
-        // Obtener la referencia a la tabla
         const tabla = $('#miTabla');
-
-        // Iterar sobre los datos y agregar filas a la tabla
         data.forEach(item => {
         tabla.bootstrapTable('append', {
             Tipo: item.Tipo,
